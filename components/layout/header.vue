eef2e190d6066cb0d559e2cc6b1a2e1b0f844b4e
Neil Yang <neil.yang@flyup.tw>
2021-03-05 16:32:20 +0800

    add images

---
+++ components/layout/header.vue
@@ -0,0 +1,62 @@
+<template>
+  <header class="sticky-top bg-nothing">
+    <div class="container">
+      <p>hello world</p>
+      <!-- Image and text -->
+      <ul class="nav mobile">
+        <!--Hamburger menu toggle button-->
+        <li class=".nav-item">
+          <a href="#" class="nav-link menu-trigger" id="menu-btn">
+            <span></span>
+            <span></span>
+            <span></span>
+          </a>
+        </li>
+        <div class="menu-container">
+          <ul>
+            <li>
+              <!-- <a class="nav-link" href="../index.html">首頁</a> -->
+            </li>
+            <li>
+              <!-- <a class="nav-link" href="product.html">甜點</a> -->
+            </li>
+            <li>
+              <!-- <a class="nav-link" href="login.html">登入</a> -->
+            </li>
+          </ul>
+        </div>
+        <li class="nav-item">
+          <!-- <a class="nav-link" href="../index.html">
+            <img src="../stylesheet/img/logotype-lg-dark.svg" alt="" width="114.14px">
+          </a> -->
+        </li>
+        <li class="nav-item">
+          <!-- <a class="nav-link" href="cart.html">
+              <img src="../stylesheet/img/baseline-shopping_cart-24px.svg" alt="">
+          </a> -->
+        </li>
+      </ul>
+      <nav class="navbar navbar-light">
+        <!-- <a class="navbar-brand" href="../index.html">
+          <img src="../stylesheet/img/logo-all-dark.svg" height="40" class="d-inline-block align-top" alt="">
+        </a> -->
+        <ul class="nav justify-content-end">
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="../index.html">首頁</a> -->
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="product.html">甜點</a> -->
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="login.html">登入</a> -->
+          </li>
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="cart.html">
+              <img src="../stylesheet/img/baseline-shopping_cart-24px.svg" alt="">
+            </a> -->
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
---
+++ layouts/front.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header />
+    <Nuxt />
+    <!-- <Footer /> -->
+  </div>
+</template>
+
+<script>
+import Header from '~/components/layout/header.vue'
+// import Footer from '~/components/layout/footer.vue'
+
+export default {
+  components: {
+    Header,
+    // Footer,
+  }
+}
+</script>
